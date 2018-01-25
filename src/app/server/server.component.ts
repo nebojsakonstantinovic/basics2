bab01ebd921ef6adc9146c944dd391ed4e71311d
Frontend <frontend@Cubess-iMac-3.local>
2018-01-25 16:54:56 +0100

    inicijalni commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+
+  serverId = 17;
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  ngOnInit() {
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+allowNewServer: boolean = false;
+serverCreationStatus: string = "There was no new server yet";
+serverCreated: boolean = false;
+newServerName: string = "name-placeholder";
+servers = ['TestServer', 'TestServer2', 'TestServer3'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+
+
+
+
+  onAddNewServer(){
+    this.serverCreationStatus = "New server has been created";
+    this.serverCreated = true;
+    this.servers.push(this.newServerName);
+  }
+
+
+
+  onUpdateServerName($event){
+    this.newServerName = $event.target.value;
+  }
+
+
+
+}
